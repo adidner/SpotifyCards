ebf5fab673107cfdf087e1cb81fb4dd25ac733dc
Aaron Didner <adidner@gmail.com>
2022-10-17 20:00:21 -0400

    Defaulting for black and white photos

--- src/BarCodeColorGeneration.py
+++ src/BarCodeColorGeneration.py
@@ -5,7 +5,7 @@ import os
 from Constants import BARCODE_WIDTH, BARCODE_HEIGHT
 
 
-def generateStoreBarCodeBackground(albumArtPath,storagePath, photoKey):
+def generateStoreBarCodeBackground(albumArtPath, storagePath, photoKey):
     print("Generate store barcode background")
 
     try:
@@ -22,14 +22,25 @@ def generateStoreBarCodeBackground(albumArtPath,storagePath, photoKey):
     g = 0
     b = 0
     size = len(by_color.keys())
+    
     for color in by_color.keys():
-        r += color[0]
-        g += color[1]
-        b += color[2]
+        if isinstance(color, tuple):
+            r += color[0]
+            g += color[1]
+            b += color[2]    
 
     r = int(r/size)
     g = int(g/size)
     b = int(b/size)
+
+
+    # with a black a white photo, default bar to white
+    if r == 0:
+        r = 255
+    if g == 0:
+        g = 255
+    if b == 0:
+        b = 255
 
     img = Image.new('RGB', (BARCODE_WIDTH, BARCODE_HEIGHT), (r, g, b))
     img.save(storagePath + "/" + photoKey, "PNG")
